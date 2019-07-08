5af817987c8826d187a91ec584197688513a68d5
Brad Brandhorst <Brad.Brandhorst@ibm.com>
2019-07-08 11:37:25 -0400

    Create routes for GET, PUT, POST, DELETE

--- routes/employee.js
+++ routes/employee.js
@@ -5,9 +5,32 @@ const router = express.Router();
 
 const DATABASE = {};
 
-/* GET employees listing. */
-router.get('', function (req, res) {
+// GET all employees
+router.get('/', function (req, res) {
     return res.send(DATABASE);
 });
+
+// GET employee by id
+router.get('/:id', function (req, res) {
+    
+});
+
+// POST new employee
+router.post('/', function (req, res) {
+
+});
+
+// PUT (Update) an employee
+router.put('/:id', function (req, res) {
+
+});
+
+// DELETE an employee
+router.delete('/:id', function (req, res) {
+
+});
+
+
+
 
 module.exports = router;
